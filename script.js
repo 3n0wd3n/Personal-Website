5df12bc45f991e15f4e242141f04a7a354218cc7
Mike <47132583+3n0wd3n@users.noreply.github.com>
2021-09-14 17:20:58 +0200

    test

--- script.js
+++ script.js
@@ -144,6 +144,7 @@ function addPosts(e){
  parentOfInputFields[2].value = "";
  console.log(arrayForBadInputs.length);
  for (var i = 0; i < arrayForBadInputs.length; i++) {
+   console.log(i);
   //  takže jsem si vytvořil cyklus kde budu chtít aby se obarvovali jen rámečky polí které nejsou vyplněné
   //  už mám hotové vyselektování těch polí které uživatel nevyplnil pod seznam arrayForBadInputs a budu obarvovat děti od parentOfInptFields rodiče
   //  musím vyřešit to aby se rozsvítili současně
